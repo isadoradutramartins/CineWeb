a7662c3c3ae1da501375c6f15b31ccda075e3207
isadoradutramartins <147355863+isadoradutramartins@users.noreply.github.com>
2023-11-10 19:11:50 -0300

    Delete tabelas_trabalho.php

--- tabelas_trabalho.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function filmes_select (){
-    global $conn;
-    $sth = $conn ->prepare("SELECT CD_FILME, NM_GENERO FROM TB_GENERO_FILME");
-    $sth->execute ();
-    return $sth->fetchAll();
-}
